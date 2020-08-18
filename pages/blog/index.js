7f04de75d84782f28f3744cafaf470bb9cf8822d
casdidier <cheung.didier@gmail.com>
2020-08-18 16:39:04 +0200

    data fetching for page content and page paths

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,17 +1,33 @@
-import Link from 'next/link'
-import Head from 'next/head'
-
-export default function FirstPost() {
-  return (
-    <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h2>
-        <Link href="/">
-          <a>Blog page</a>
-        </Link>
-      </h2>
-    </>
-  )
+function Blog({ posts }) {
+  // Render posts...
 }
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://.../posts')
+  const posts = await res.json()
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
+
+  // This function gets called at build time
+  export async function getStaticPaths() {
+    // Call an external API endpoint to get posts
+    const res = await fetch('https://.../posts')
+    const posts = await res.json()
+
+    // Get the paths we want to pre-render based on posts
+    const paths = posts.map((post) => `/posts/${post.id}`)
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+  }
+
+  export default Blog
